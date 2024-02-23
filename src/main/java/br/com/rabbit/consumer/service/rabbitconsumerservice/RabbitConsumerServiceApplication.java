200d0a55bbab70ed774aa4b84fd6cdf92d1f654b
Ryan Capurso <ryan_capurso@hotmail.com>
2024-02-23 16:49:43 -0300

    Inicio de consumidor String

---
+++ src/main/java/br/com/rabbit/consumer/service/rabbitconsumerservice/RabbitConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.rabbit.consumer.service.rabbitconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitConsumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitConsumerServiceApplication.class, args);
+	}
+
+}
